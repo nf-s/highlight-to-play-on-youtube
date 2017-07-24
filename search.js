575e4c8e35e464781af2cdee9748f7ac7bf22705
nf-s <nick@forbes-smith.com>
2017-07-24 15:23:20 +1000

    Added json file for api key

--- search.js
+++ search.js
@@ -14,6 +14,7 @@ function search() {
 
   request.execute(function(response) {
     var str = JSON.stringify(response.result);
+    console.log(response)
     var video = "https://www.youtube.com/embed/"+response.result.items[1].id.videoId+"?autoplay=1";
     //$('#results').html('<pre>' + response.result.items[1].id.videoId + '</pre>');
     $('#ytplayer').attr("src", video);
@@ -21,9 +22,13 @@ function search() {
 }
 
 function start() {
+  apikey = ""
+  $.getJSON("config.json", function(json) {
+    apikey = json.apikey
+  });
   // Initializes the client with the API key and the Translate API.
   gapi.client.init({
-    'apiKey': 'nope',
+    'apiKey': apikey,
     'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'],
   }).then(function () {
     search()
